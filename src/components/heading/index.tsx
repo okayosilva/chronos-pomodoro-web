b661ed8fc3b08befb11ec48bcdb882230aed4fbb
KayoSilva <kayo16silva@gmail.com>
2025-06-11 14:55:34 -0300

    style: add dark theme

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,6 +1,3 @@
-import styles from './heading.module.css';
-
 export function Heading({ children }: { children: React.ReactNode }) {
-  const classes = `${styles.heading} ${styles.heading2}`;
-  return <h1 className={classes}>{children}</h1>;
+  return <h1>{children}</h1>;
 }
