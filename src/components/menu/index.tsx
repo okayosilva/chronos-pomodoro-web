7f97c2cc2af3896c76d0397ec3d15537e27b0444
KayoSilva <kayo16silva@gmail.com>
2025-06-16 23:51:15 -0300

    feat: implement theme toggle functionality in menu component and update styles for light theme

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,23 +1,69 @@
-import { History, Home, Settings, Sun } from 'lucide-react';
+import { History, Home, Moon, Settings, Sun } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 
+type AvailableThemes = 'light' | 'dark';
+
 export function Menu() {
+  const [currentTheme, setCurrentTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('@chronos:theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  const handleToggleTheme = () => {
+    setCurrentTheme((prevTheme) => {
+      const nextTheme = prevTheme === 'light' ? 'dark' : 'light';
+      return nextTheme;
+    });
+  };
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = currentTheme || 'light';
+    localStorage.setItem('@chronos:theme', currentTheme);
+  }, [currentTheme]);
+
   const menuItens = [
-    { label: 'Home', href: '/', icon: <Home /> },
-    { label: 'History', href: '/history', icon: <History /> },
-    { label: 'Settings', href: '/settings', icon: <Settings /> },
-    { label: 'Sun', href: '/sun', icon: <Sun /> },
+    { label: 'Home', href: '/', icon: <Home />, arialLabel: 'Página Inicial' },
+    {
+      label: 'History',
+      href: '/history',
+      icon: <History />,
+      arialLabel: 'Página Histórico',
+    },
+    {
+      label: 'Settings',
+      href: '/settings',
+      icon: <Settings />,
+      arialLabel: 'Página Configurações',
+    },
   ];
 
   return (
     <nav className={styles.menu}>
       {menuItens.map((item) => {
         return (
-          <a key={item.label} href={item.href} className={styles.menuLink}>
+          <a
+            key={item.label}
+            href={item.href}
+            className={styles.menuLink}
+            aria-label={item.arialLabel}
+            title={item.arialLabel}
+          >
             {item.icon}
           </a>
         );
       })}
+
+      <button
+        type='button'
+        className={styles.menuLink}
+        onClick={handleToggleTheme}
+        title='Mudar tema'
+        aria-label='Mudar tema'
+      >
+        {currentTheme === 'light' ? <Sun /> : <Moon />}
+      </button>
     </nav>
   );
 }
